45a8a269939863fc2973a7e34d21eeb8bae685e7
Bram <59796107+BramOtte@users.noreply.github.com>
2022-12-02 17:22:05 +0100

    make build script work with deno

--- build.cmd
+++ build.cmd
@@ -1,1 +1,2 @@
-esbuild src/compiler.ts --outdir=target --bundle --format=esm --sourcemap %1
+@REM esbuild src/compiler.ts --outdir=target --bundle --format=esm --sourcemap %1
+deno bundle src/compiler.ts -- target/compiler.js
