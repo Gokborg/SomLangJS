9f1c6ab01da37b63303cdb6aec5a2952d84a418b
Bram <59796107+BramOtte@users.noreply.github.com>
2022-12-02 17:40:05 +0100

    edit build script to allow for --watch

--- build.cmd
+++ build.cmd
@@ -1,2 +1,2 @@
 @REM esbuild src/compiler.ts --outdir=target --bundle --format=esm --sourcemap %1
-deno bundle src/compiler.ts -- target/compiler.js
+deno bundle src/compiler.ts %* -- target/compiler.js
