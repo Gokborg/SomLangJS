825f6d055d45c0135573664c65e83614c1d1f714
Gokborg <gokborg2@gmail.com>
2022-12-07 18:09:34 +0530

    Dereferencing array elements

--- src/codegen.ts
+++ src/codegen.ts
@@ -369,6 +369,24 @@ export class CodeGeneration {
                 this.allocator.setFreeRegister(reg);
                 return varReg; 
             }
+            else if(expr.iner instanceof ast.ArrayAccess) {
+                const regIndex: number = this.genExpression(expr.iner.index);
+                if (!(expr.iner.array instanceof ast.Identifier)) {
+                    throw new Error(`array access is not implemented for ${expr.iner.array}`);
+                }
+                const memArray: number = this.allocator.hasVariable(expr.iner.array.token.value);
+                const regArray: number = this.allocator.getFreeRegister();
+                this.asm.putLI(regArray, memArray);
+                this.asm.putADD(regIndex, regArray, regIndex);
+
+                //regIndex now holds the mem location of the array element
+                this.asm.putLOADWITHREG(regIndex, regIndex);
+                this.asm.putLOADWITHREG(regIndex, regIndex);
+
+                this.allocator.setFreeRegister(regArray);
+                
+                return regIndex;
+            }
             else {
                 return -1;
             }
